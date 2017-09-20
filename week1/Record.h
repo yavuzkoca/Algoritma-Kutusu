860793847938685f71b161860b7cea7c134df544
alitolga <alitolga.dincer@gmail.com>
2017-09-21 01:49:50 +0300

    Add files via upload

---
+++ week1/Record.h
@@ -0,0 +1,11 @@
+#pragma once
+
+#include <cstdio>
+#include "stdafx.h"
+
+#define AD_UZUNLUK 30
+#define TELNO_UZUNLUK 15
+struct Tel_Kayit {
+	char ad[AD_UZUNLUK];
+	char telno[TELNO_UZUNLUK];
+};
--- week1/fileoperations.h
+++ week1/fileoperations.h
@@ -1,15 +1,17 @@
+#pragma once
+
 #ifndef DOSYAISLEMLERI_H
 #define DOSYAISLEMLERI_H
-#include <stdio.h>
-#include "record.h"
-struct Dosya{
-    char *dosyaadi;
-    FILE *teldefteri;
-    void olustur();
-    void kapat();
-    void ekle(Tel_Kayit *);
-    int ara(char []);
-    void sil(int kayitno);
-    void guncelle(int kayitno, Tel_Kayit *);
+#include <cstdio>
+#include "Record.h"
+struct Dosya {
+	char *dosyaadi;
+	FILE *teldefteri;
+	void olustur();
+	void kapat();
+	void ekle(Tel_Kayit *);
+	int ara(char[]);
+	void sil(int kayitno);
+	void guncelle(int kayitno, Tel_Kayit *);
 };
 #endif
