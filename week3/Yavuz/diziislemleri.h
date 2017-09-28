5df1a929b18831f0397a880b9cb917a9fa5b44b7
Yavuz KOCA <ykoca0808@gmail.com>
2017-09-28 11:33:16 +0300

    3. hafta labda yazdığım kodlar

--- week3/Yavuz/diziislemleri.h
+++ week3/Yavuz/diziislemleri.h
@@ -8,19 +8,24 @@
 #define MAXKAYIT 100
 
 struct Dizi{
-    char *dosyaadi;
-    FILE *teldefteri;
-    Tel_Kayit k[MAXKAYIT];
-    void olustur();
-    void kapat();
-    void ekle(Tel_Kayit *);
-    int ara(char []);
-    void sil(int kayitno);
-    void guncelle(int kayitno, Tel_Kayit *);
-    void listele();
-    int kayitsayisi;
+	char *dosyaadi;
+	char temp[30];
+	char n[30];
+	char num[30];
+	FILE *teldefteri;
+	Tel_Kayit k[MAXKAYIT];
+	void olustur();
+	void kapat();
+	void ekle(Tel_Kayit *);
+	int ara(char[]);
+	void sil(int kayitno);
+	void guncelle(int kayitno, Tel_Kayit *);
+	void listele();
+	void sortData();
+	int kayitsayisi;
+	void Dizi::writeToFile();
 };
 
 
 
-#endif //C_C_DÄ°ZÄ°Ä°SLEMLERÄ°_H
+#endif //C_C_DÝZÝÝSLEMLERÝ_H
--- week3/Yavuz/kayit.h
+++ week3/Yavuz/kayit.h
@@ -4,8 +4,8 @@
 #define AD_UZUNLUK 30
 #define TELNO_UZUNLUK 15
 struct Tel_Kayit{
-    char ad[AD_UZUNLUK];
-    char telno[TELNO_UZUNLUK];
+	char ad[AD_UZUNLUK];
+	char telno[TELNO_UZUNLUK];
 };
 
 #endif //C_C_KAYIT_H
