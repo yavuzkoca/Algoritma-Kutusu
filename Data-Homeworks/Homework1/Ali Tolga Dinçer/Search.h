fcddd532ffdd568b21857f08c058f0b6c9a6d5f0
alitolga <alitolga.dincer@gmail.com>
2017-10-11 21:59:22 +0300

    Add files via upload
    HW1 Kodlarım :)

---
+++ Data-Homeworks/Homework1/Ali Tolga Dinçer/Search.h
@@ -0,0 +1,9 @@
+#ifndef SEARCH_H
+#define SEARCH_H
+
+bool verticalSearch(char** puzzleArray, searchedWord* word);
+bool horizontalSearch(char** puzzleArray, searchedWord* word);
+bool diagonalSearch(char** puzzleArray, searchedWord* word);
+
+
+#endif
---
+++ Data-Homeworks/Homework1/Ali Tolga Dinçer/Word.h
@@ -0,0 +1,11 @@
+#ifndef WORD_H
+#define WORD_H
+
+struct searchedWord {
+	int id;
+	char* word;
+	int wordLength;
+};
+
+
+#endif
